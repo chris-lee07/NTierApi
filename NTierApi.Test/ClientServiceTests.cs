f148da25d1e4f112dec651f91e1449c8a4797398
Chris Lee <chrislee0793@gmail.com>
2023-08-30 20:10:45 -0400

    Unit tests passing for ClientService

--- NTierApi.Test/ClientServiceTests.cs
+++ NTierApi.Test/ClientServiceTests.cs
@@ -1,61 +1,122 @@
 using AutoFixture;
 using AutoFixture.AutoMoq;
-using Castle.Core.Logging;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Moq;
 using NTierApi.Business;
 using NTierApi.Data;
 using NTierApi.Data.Models;
+using NTierApi.Data.Repositories;
 
 namespace NTierApi.Test
 {
     public class ClientServiceTests
     {
-        private readonly IClientService _clientService;
-        private readonly Mock<ClientContext> _clientContext;
+        private readonly IFixture _fixture;
+        private readonly Mock<IClientRepository> _clientRepo;
         private readonly Mock<ILogger<ClientService>> _logger;
 
         private ClientDbo SomeClientDbo;
-        private ClientDbo SomeOtherClientDbo;
         private const int SomeClientId = 123;
         private const int SomeOtherClientId = 456;
-        private const int SomeRandomClientId = 789;
+        private const string SomeClientName = "SomeClientName";
+        private const string SomeOtherClientName = "SomeOtherClientName";
 
         public ClientServiceTests()
         {
-            var fixture = new Fixture().Customize(new AutoMoqCustomization());
-
-            _clientContext = fixture.Freeze<Mock<ClientContext>>();
-            _logger = fixture.Freeze<Mock<ILogger<ClientService>>>();
+            _fixture = new Fixture().Customize(new AutoMoqCustomization() { ConfigureMembers = true });
+            _fixture.Inject<ClientDbo>(null);
+            _logger = _fixture.Freeze<Mock<ILogger<ClientService>>>();
+            _clientRepo = _fixture.Freeze<Mock<IClientRepository>>();
         }
 
         [SetUp]
         public void Setup()
         {
-            var fixture = new Fixture().Customize(new AutoMoqCustomization());
-            SomeClientDbo = fixture.Build<ClientDbo>().With(client => client.ClientId, SomeClientId).Create();
-            SomeOtherClientDbo = fixture.Build<ClientDbo>().With(client => client.ClientId, SomeOtherClientId).Create();
+            SomeClientDbo = _fixture.Build<ClientDbo>().With(client => client.ClientId, SomeClientId).With(client => client.ClientName, SomeClientName).Create();
         }
 
         [Test]
-        public async Task ReturnCorrectClient()
+        public async Task ReturnCorrectClientByClientId()
         {
             // Arrange
-            var fixture = new Fixture().Customize(new AutoMoqCustomization());
-
-            var logger = fixture.Freeze<Mock<ILogger<ClientService>>>();
-            var clientContext = fixture.Freeze<Mock<ClientContext>>();
-            var clientDbSet = fixture.Freeze<Mock<DbSet<ClientDbo>>>();
-            clientDbSet.Setup(cds => cds.FirstOrDefaultAsync(c => c.ClientId == SomeClientId, It.IsAny<CancellationToken>())).ReturnsAsync(SomeClientDbo);
-            clientContext.Setup(cc => cc.Clients).Returns(clientDbSet.Object);
-            var clientService = fixture.Create<ClientService>();
+            var logger = _fixture.Freeze<Mock<ILogger<ClientService>>>();
+            var clientRepo = _fixture.Freeze<Mock<IClientRepository>>();
+            clientRepo.Setup(cr => cr.GetByClientId(SomeClientId)).ReturnsAsync(SomeClientDbo);
+            var clientService = _fixture.Create<ClientService>();
 
             // Act
             var client = await clientService.GetByClientId(SomeClientId);
 
             // Assert
             Assert.That(client.ClientId, Is.EqualTo(SomeClientId));
+            Assert.That(client.ClientName, Is.EqualTo(SomeClientName));
+        }
+
+        [Test]
+        public async Task ReturnsNoClientByClientId()
+        {
+            // Arrange
+            var logger = _fixture.Freeze<Mock<ILogger<ClientService>>>();
+            var clientRepo = _fixture.Freeze<Mock<IClientRepository>>();
+            clientRepo.Setup(cr => cr.GetByClientId(SomeClientId)).ReturnsAsync(SomeClientDbo);
+            var clientService = _fixture.Create<ClientService>();
+
+            // Act
+            var client = await clientService.GetByClientId(SomeOtherClientId);
+
+            // Assert
+            Assert.IsNull(client);
+        }
+
+        [Test]
+        public async Task ReturnCorrectClientByClientName()
+        {
+            // Arrange
+            var logger = _fixture.Freeze<Mock<ILogger<ClientService>>>();
+            var clientRepo = _fixture.Freeze<Mock<IClientRepository>>();
+            clientRepo.Setup(cr => cr.GetByClientByName(SomeClientName)).ReturnsAsync(SomeClientDbo);
+            var clientService = _fixture.Create<ClientService>();
+
+            // Act
+            var client = await clientService.GetByClientByName(SomeClientName);
+
+            // Assert
+            Assert.That(client.ClientId, Is.EqualTo(SomeClientId));
+            Assert.That(client.ClientName, Is.EqualTo(SomeClientName));
+        }
+
+        [Test]
+        public async Task ReturnsNoClientByClientName()
+        {
+            // Arrange
+            var logger = _fixture.Freeze<Mock<ILogger<ClientService>>>();
+            var clientRepo = _fixture.Freeze<Mock<IClientRepository>>();
+            clientRepo.Setup(cr => cr.GetByClientByName(SomeClientName)).ReturnsAsync(SomeClientDbo);
+            var clientService = _fixture.Create<ClientService>();
+
+            // Act
+            var client = await clientService.GetByClientByName(SomeOtherClientName);
+
+            // Assert
+            Assert.IsNull(client);
+        }
+
+        [Test]
+        public async Task ReturnsAllClients()
+        {
+            // Arrange
+            int clientCount = 5;
+            var logger = _fixture.Freeze<Mock<ILogger<ClientService>>>();
+            var clientRepo = _fixture.Freeze<Mock<IClientRepository>>();
+            var clients = _fixture.Build<ClientDbo>().CreateMany(clientCount).ToList();
+            clientRepo.Setup(cr => cr.GetClients()).ReturnsAsync(clients);
+            var clientService = _fixture.Create<ClientService>();
+
+            // Act
+            var results = await clientService.GetClients();
+
+            // Assert
+            Assert.That(results.Count(), Is.EqualTo(clientCount));
         }
     }
 }
